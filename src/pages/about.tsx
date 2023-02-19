d62438fe24e8d39f65af4ffba54c31c38615ae62
EasySimpleCool <easysimplecool@gmail.com>
2023-02-20 01:28:04 +1100

    homepage

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex">
+        <div>icon</div>
+        <div>welcome</div>
+        <div>nav</div>
+      </div>
+
+    </>
+
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className="flex flex-col h-screen justify-between p-10">
+        <div className='flex bg-slate-900'><div>icon</div></div>
+        <div className='flex grow justify-center items-center bg-slate-800'><h1>welcome</h1></div>
+        <div className='flex bg-slate-900 justify-end'>
+          <div className='flex'>
+            <div>design</div>
+            <div>print</div>
+            <div>code</div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
