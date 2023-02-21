69e6e19bea12e55d60df99759ac99101101a7472
EasySimpleCool <easysimplecool@gmail.com>
2023-02-21 22:04:24 +1100

    asd

--- app/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function NotesPage() {
-    return (
-        <div>
-            <h1>Notes Page</h1>
-            <p>Some content</p>
-        </div>
-    );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
+            <h2>Heading2</h2>
             <p>Some content</p>
         </div>
     )
